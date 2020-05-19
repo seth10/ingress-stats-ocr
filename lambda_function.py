575016987b8c0cb044c8c55745900eb44db4cd80
Seth T <seth10@users.noreply.github.com>
2020-05-19 15:54:00 -0700

    Accepting multiple screenshots from Shortcuts
    Now using the Shortcuts app on my iPhone I can pull the latest 3
    screenshots, crop them, base64 encode them, and make an API call to this
    Lambda via API Gateway.

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,21 @@
 import pytesseract
 import PIL.Image
 import io
+import json
 import os
 from base64 import b64decode
 
 
 LAMBDA_TASK_ROOT = os.environ.get('LAMBDA_TASK_ROOT', os.path.dirname(os.path.abspath(__file__)))
 os.environ["PATH"] += os.pathsep + LAMBDA_TASK_ROOT
-#os.environ["LD_LIBRARY_PATH"] = os.path.join(LAMBDA_TASK_ROOT, 'tessdata')
 os.environ["TESSDATA_PREFIX"] = os.path.join(LAMBDA_TASK_ROOT, 'tessdata')
 
 def lambda_handler(event, context):
-  print os.getcwd()
-  print LAMBDA_TASK_ROOT
-  print os.listdir('.')
-  print os.environ["LD_LIBRARY_PATH"]
-  print os.environ["TESSDATA_PREFIX"]
-  binary = b64decode(event['image64'])
-  image = PIL.Image.open(io.BytesIO(binary))
-  text = pytesseract.image_to_string(image)
-  return {'text' : text}
+  ret = {}
+  for screenshotName, image64 in json.loads(event['body']).items():
+    print screenshotName
+    binary = b64decode(image64)
+    image = PIL.Image.open(io.BytesIO(binary))
+    text = pytesseract.image_to_string(image)
+    ret[screenshotName] = text
+  return ret
