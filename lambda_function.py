79861adb5fe07c125e9d1666053998a05392b41d
Seth T <seth10@users.noreply.github.com>
2020-05-19 16:23:00 -0700

    Cleanup text and combine All Time screenshots
    Not all of the stats from the ALL TIME page in Ingress fit on one
    screenshot, so I have to take two. This leads to some data being
    present in both screenshots, leading to me removing the duplicate part.

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 import pytesseract
 import PIL.Image
 import io
@@ -11,11 +13,22 @@ os.environ["PATH"] += os.pathsep + LAMBDA_TASK_ROOT
 os.environ["TESSDATA_PREFIX"] = os.path.join(LAMBDA_TASK_ROOT, 'tessdata')
 
 def lambda_handler(event, context):
-  ret = {}
+  t = {}
   for screenshotName, image64 in json.loads(event['body']).items():
-    print screenshotName
     binary = b64decode(image64)
     image = PIL.Image.open(io.BytesIO(binary))
     text = pytesseract.image_to_string(image)
-    ret[screenshotName] = text
-  return ret
+    t[screenshotName] = text
+  
+  for i in t:
+    t[i] = t[i].replace("—", "-").replace("MUS", "MUs").replace("\n\n", "\n")
+  
+  now = t['now']
+  alltime = t['alltime1']
+  duplicateLength = 1
+  while alltime.rfind(t['alltime2'][:duplicateLength]) > -1:
+    duplicateLength += 1
+  alltime += t['alltime2'][duplicateLength-1:]
+
+
+  return "NOW\n{}\n\nALL TIME\n{}".format(now, alltime)
